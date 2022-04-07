88f3b8533c7797acbf411dc1729d08362cfb5c6a
David Wenzel <dwenzel95@gmail.com>
2022-04-07 18:52:00 -0500

    added node getters

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,16 @@
 // This is for javaScript
+
+// STOPWATCH
+//vars
+
+let seconds = 00;
+let tens = 00;
+let appendTens = document.getElementById('tens');
+let appendSeconds = document.getElementById('seconds');
+const buttonStart = document.getElementById('button-start');
+const buttonStop = document.getElementById('button-stop');
+
+//STOPWATCH FUNCTIONALITY
+function startTimer(){
+}
+
