2dc32dfecee1f08f6f99071a92cef187a03dc86c
David Wenzel <dwenzel95@gmail.com>
2022-04-24 08:59:39 -0500

    created about click event

--- javascripts/index.js
+++ javascripts/index.js
@@ -113,4 +113,17 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     loadWod()
 
+    // about button click
+    const aboutLink = document.getElementById('about')
+    
+    const renderAboutPage = (e) => {
+        e.preventDefault();
+        alert('hi yayayayay')
+    }
+    
+    const attachAboutLinkEvent = () => {
+        aboutLink.addEventListener('click', renderAboutPage)
+    }
+    
+    attachAboutLinkEvent()
 });
